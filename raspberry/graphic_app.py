8b3d9e9795d89505ff152e5ecd3d9b1aa00e594a
finkin <arlekias@gmail.com>
2023-01-08 23:21:24 +0100

    Big refactor

--- raspberry/graphic_app.py
+++ raspberry/graphic_app.py
@@ -1,180 +1,134 @@
 from matplotlib import pyplot as plt
 import csv
 import glob
+from enum import Enum
+from pick import pick
+
+
+class MenuOption(Enum):
+    TEMPERATURE = 0
+    PRESSURE = 1
+    FLOW = 2
+    RPM = 3
+    THROTTLE = 4
+    FUEL = 5
+    CUSTOM = 6
+
+
+class Headers(Enum):
+    TEMP1 = 0
+    TEMP2 = 1
+    TEMP3 = 2
+    TEMP4 = 3
+    PRESS1 = 4
+    PRESS2 = 5
+    FLOW1 = 6
+    RPM = 7
+    THROTTLE = 8
+    FUEL = 9
+    TIME = 10
+
+
+colors = [
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "yellow",
+    "purple",
+    "pink",
+    "black",
+    "gray",
+    "brown"
+]
+
+
+def show_file_selector():
+    title = "Choose source file:"
+    options = glob.glob("./*.csv")
+    option, _ = pick(options, title, indicator="=>")
+    return option
+
+
+def show_menu():
+    title = "Input data to graph:"
+    options = ["Temperatures", "Pressures", "Flow", "RPM", "Throttle", "Fuel level", "Custom"]
+    _, index = pick(options, title, indicator="=>")
+    return index
+
+
+def main():
+    filename = show_file_selector()
+    menu_indicator = show_menu()
+    graph(menu_indicator, filename)
+
+
+def plot(x, values):
+    color = 0
+    _, ax = plt.subplots()
+    for value in values:
+        ax.plot(x, value, color=colors[color], linewidth=2)
+        color += 1
+    plt.show()
+
+
+def graph(menu_indicator, filename):
+
+    with open(f"{filename}") as f:
+        reader = csv.reader(f)
+        data = list(reader)
+
+    x = [float(row[Headers.TIME.value]) for row in data]
+
+    match menu_indicator:
+        case MenuOption.TEMPERATURE.value:
+            values = [
+                [float(row[Headers.TEMP1.value]) for row in data],
+                [float(row[Headers.TEMP2.value]) for row in data],
+                [float(row[Headers.TEMP3.value]) for row in data],
+                [float(row[Headers.TEMP4.value]) for row in data],
+            ]
+
+        case MenuOption.PRESSURE.value:
+            values = [
+                [float(row[Headers.PRESS1.value]) for row in data],
+                [float(row[Headers.PRESS2.value]) for row in data],
+            ]
+
+        case MenuOption.FLOW.value:
+            values = [[float(row[Headers.FLOW1.value]) for row in data]]
+
+        case MenuOption.RPM.value:
+            values = [[float(row[Headers.RPM.value]) for row in data]]
+
+        case MenuOption.THROTTLE.value:
+            values = [[float(row[Headers.THROTTLE.value]) for row in data]]
 
+        case MenuOption.FUEL.value:
+            values = [[float(row[Headers.FUEL.value]) for row in data]]
 
-while True:
+        case MenuOption.CUSTOM.value:
 
-    print(glob.glob("./*.csv"))
-    filename = input("Enter file to graph(Y-M-D_H-M-S)")
+            title = "Select main data to compare to:"
+            options = [
+                "TEMP1", "TEMP2", "TEMP3", "TEMP4",
+                "PRESS1", "PRESS2", "FLOW1", "RPM",
+                "THROTTLE", "FUEL", "TIME"
+            ]
+            _, index = pick(options, title, indicator="=>")
+            x = [float(row[index]) for row in data]
 
-    print("Menu:")
-    print("1. --Temperatures--")
-    print("2. ----Pressures---")
-    print("3. ------Flow------")
-    print("4. ------RPM-------")
-    print("5. ----Throttle----")
-    print("6. ---Fuel level---")
-    print("7. -----Custom-----")
-    print("   ----------------")
+            title = 'Select additional data to compare: '
+            selected = pick(options, title, multiselect=True, min_selection_count=1)
 
-    menu_indicator = input("Input data to graph:")
+            values = []
+            for s in selected:
+                values.append([float(row[s[1]]) for row in data])
 
-    if menu_indicator == "1":   #TEMPERATURE (4)
+    plot(x, values)
 
-        val1 = 0
-        val2 = 1
-        val3 = 2
-        val4 = 3
-        val5 = 10
 
-        with open(f'{filename}.csv') as f:
-            reader = csv.reader(f)
-            data = list(reader)
+if __name__ == "__main__":
 
-        values1 = [float(row[val1]) for row in data]
-        values2 = [float(row[val2]) for row in data]
-        values3 = [float(row[val3]) for row in data]
-        values4 = [float(row[val4]) for row in data]
-        values5 = [float(row[val5]) for row in data]
-
-        fig, ax = plt.subplots()
-
-        ax.plot(values5, values1, color = 'blue' , linewidth = 2)
-        ax.plot(values5, values2, color = 'red' , linewidth = 2)
-        ax.plot(values5, values3, color = 'green' , linewidth = 2)
-        ax.plot(values5, values4, color = 'yellow' , linewidth = 2)
-
-        plt.show()
-
-
-    if menu_indicator == "2":   #PRESSURE (2)
-
-        val1 = 4
-        val2 = 5
-        val3 = 10
-
-        with open(f'{filename}.csv') as f:
-            reader = csv.reader(f)
-            data = list(reader)
-
-        values1 = [float(row[val1]) for row in data]
-        values2 = [float(row[val2]) for row in data]
-        values3 = [float(row[val3]) for row in data]
-
-        fig, ax = plt.subplots()
-
-        ax.plot(values3, values1, color = 'blue' , linewidth = 2)
-        ax.plot(values3, values2, color = 'red' , linewidth = 2)
-
-        plt.show()
-
-    if menu_indicator == "3":    #FLOW
-
-        val1 = 6
-        val2 = 10
-
-        with open(f'{filename}.csv') as f:
-            reader = csv.reader(f)
-            data = list(reader)
-
-        values1 = [float(row[val1]) for row in data]
-        values2 = [float(row[val2]) for row in data]
-
-        fig, ax = plt.subplots()
-
-        ax.plot(values2, values1, color = 'blue' , linewidth = 2)
-
-        plt.show()
-
-
-    if menu_indicator == "4":    #RPM
-
-        val1 = 7
-        val2 = 10
-
-        with open(f'{filename}.csv') as f:
-            reader = csv.reader(f)
-            data = list(reader)
-
-        values1 = [float(row[val1]) for row in data]
-        values2 = [float(row[val2]) for row in data]
-
-        fig, ax = plt.subplots()
-
-        ax.plot(values2, values1, color = 'blue' , linewidth = 2)
-
-        plt.show()
-
-
-    if menu_indicator == "5":    #THROTTLE
-
-        val1 = 8
-        val2 = 10
-
-        with open(f'{filename}.csv') as f:
-            reader = csv.reader(f)
-            data = list(reader)
-
-        values1 = [float(row[val1]) for row in data]
-        values2 = [float(row[val2]) for row in data]
-
-        fig, ax = plt.subplots()
-
-        ax.plot(values2, values1, color = 'blue' , linewidth = 2)
-
-        plt.show()
-
-
-    if menu_indicator == "6":    #FUEL
-
-        val1 = 9
-        val2 = 10
-
-        with open(f'{filename}.csv') as f:
-            reader = csv.reader(f)
-            data = list(reader)
-
-        values1 = [float(row[val1]) for row in data]
-        values2 = [float(row[val2]) for row in data]
-
-        fig, ax = plt.subplots()
-
-        ax.plot(values2, values1, color = 'blue' , linewidth = 2)
-
-        plt.show()
-
-
-    if menu_indicator == "7":    #CUSTOM
-
-        number_of_values = int(input("Select number of values to compare"))   #preguntar por el número de variables para los bucles
-
-        variables = {}                                                        #diccionario para cada variable con su valor
-        principal_value = int(input("Select principal value:"))               #guardar variable principal a la que comparar todos los datos
-
-        for i in range(number_of_values-1):
-            variables[f'val{i}'] = int(input(f'Select value {i+1}:'))         #Guardar cada variable con el valor deseado
-
-        with open(f'{filename}.csv') as f:                                    #Abrir y preparar lectura de datos
-            reader = csv.reader(f)
-            data = list(reader)
-
-        values_list = {}
-        for i in range(number_of_values-1):
-            values_list[f'values{i}'] = [float(row[f'val{i}']) for row in data]                         #Guardar los valores del documento en cada variable
-
-        fig, ax = plt.subplots()
-
-        list_of_colors = ["red", "blue", "green", "orange", "yellow", "purple", "pink", "black"]        #Lista para variar los colores de las gráficas
-
-        for i in range(number_of_values-1):
-
-            ax.plot(principal_value, f'values{i}', color = list_of_colors[i] , linewidth = 2)           #Imprimir cada gráfica
-
-        plt.show()
-
-
-        
-
-
-
+    # while True:
+    main()
